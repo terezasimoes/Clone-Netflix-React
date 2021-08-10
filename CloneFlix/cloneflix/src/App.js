bb296940eeb700b021fc58a9f7ed23fb99251f88
Tereza Crisitina <terezasimoes@gmail.com>
2021-08-10 00:24:19 -0300

    movie list on browser console

--- CloneFlix/cloneflix/src/App.js
+++ CloneFlix/cloneflix/src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import MoviesDB from './MoviesDB';
 
 export default () => {
+  
+  useEffect(() => {
+    const loadMovies = async () => {
+      let movies = await MoviesDB.getHomeMovies();
+      console.log(movies)
+    }
+    loadMovies();
+  }, [])
+
 
   return (
     <div>
