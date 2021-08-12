4a2801af6188216e52f8e51a321570e5c9ddb8b4
Tereza Crisitina <terezasimoes@gmail.com>
2021-08-12 00:12:22 -0300

    css effects on movieLine

--- CloneFlix/cloneflix/src/components/MovieLine.js
+++ CloneFlix/cloneflix/src/components/MovieLine.js
@@ -3,16 +3,19 @@ import './MovieLine.css';
 
 export default ({title, items}) => {
   return (
-    <div>
+    <div className="movieRow--container">
 		  <h2>{title}</h2>
 			<div className="movieRow--linearea">
-				{items.results.length > 0 && items.results.map((elem, index) => (
-					<img 
-						key={index}
-						src={`http://image.tmdb.org/t/p/w300${elem.poster_path}`}
-						alt={elem.original_title}
-					/>
-				))}
+				<div className="movieRow--list">
+					{items.results.length > 0 && items.results.map((elem, index) => (
+						<div key={index} className="movieRow--item">
+							<img 
+								src={`http://image.tmdb.org/t/p/w300${elem.poster_path}`}
+								alt={elem.original_title}
+							/>
+						</div>
+					))}
+				</div>
 			</div>
     </div>
   );
